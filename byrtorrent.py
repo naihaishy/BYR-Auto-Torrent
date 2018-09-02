a1185f040c46b02458eab6f92c67adee6ae8f58e
naihai‘git config --global user.email 448435279@qq.comgit clone git@github.zhfsky.com:naihai/watermarker.gitcd watermarklstouch REAMDEMDgit config --global user.name naihai‘ <448435279@qq.com>
2018-09-02 11:21:56 +0800

    store already downloaded torrent id to log file

--- byrtorrent.py
+++ byrtorrent.py
@@ -9,6 +9,7 @@ import time
 '''
 北邮人PT自动下载脚本
 自动下载免费文件 调用系统默认BT客户端打开 
+保存已经下载的id 避免重复
 '''
 
 
@@ -83,11 +84,44 @@ def get_torrents_list():
     return torrents
 
 
+# 查看是否已经下载过了
+def view_down_log():
+    log_file = "./data/index.json"
+    if not os.path.exists(log_file):
+        return
+    else:
+        with open("./data/index.json", "r") as f:
+            lines = f.readlines()
+            log_lines = [line.replace("\n", "") for line in lines]
+            return log_lines
+
+
+# 保存已经下载的torrent
+def save_down_log(seed_id):
+    log_file = "./data/index.json"
+    if not os.path.exists(log_file):
+        f = open(log_file, "x")
+        f.write(seed_id + "\n")
+        f.close()
+    else:
+        downed_lines = view_down_log()
+        if downed_lines is not None and seed_id in downed_lines:
+            return
+        else:
+            f = open(log_file, "a")
+            f.write(seed_id + "\n")
+            f.close()
+
+
 # 排序 获取最佳的seed_torrent
 def sort_torrents_list(torrents):
     latest_time = 0.0
     latest_torrent = None
+    downed_lines = view_down_log()
+    print(downed_lines)
     for torrent in torrents:
+        if downed_lines is not None and torrent.seed_id in downed_lines:
+            continue
         ts = time.strptime(torrent.upload_time, "%Y-%m-%d %H:%M:%S")
         time_staps = time.mktime(ts)
         if time_staps > latest_time:
@@ -97,12 +131,11 @@ def sort_torrents_list(torrents):
 
 
 # 下载文件并启动系统默认BT客户端打开该文件
-def download_torrent(torrents):
-
-    title = str(torrents.title)
+def download_torrent(torrent):
+    title = str(torrent.title)
     if len(title) >= 180:
         title = title[0:179]
-        
+
     remove_list = "?><\/:\"*|"
     for ch in remove_list:
         if ch in title:
@@ -112,9 +145,12 @@ def download_torrent(torrents):
 
     headers = {'User-Agent': 'Mozilla/5.0'}
     cookies = get_cookie()
-    r = requests.get(url=torrents.download_url, headers=headers, cookies=cookies)
+    r = requests.get(url=torrent.download_url, headers=headers, cookies=cookies)
     with open(filename, "wb") as f:
         f.write(r.content)
+
+    # 保存到记录文件中
+    save_down_log(torrent.seed_id)
 
     os.startfile(os.getcwd() + filename)
 
